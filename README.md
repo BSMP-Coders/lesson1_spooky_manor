5f7478087cfd8f8a9cc276162a8c54709554680c
Shane Neuville <shane94@hotmail.com>
2024-07-06 23:32:12 +0000

    Add spooky files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lesson1_spooky_manor
 Spooky Manor Lesson 1 for Houston Advanced Coders
+
+# Reference material
+
+[Intro to Python](https://vscode.dev/edu?courseId=intro-to-python&workspace-scheme=vscode-edu-workspace&profile=default)
+
+[Homework](https://vscodeedu.com/courses/event-2023-hoc) - Work through the Spooky Manor Tutorial
+
+[Homework 2.0](my-spookymanor.ph) Build your own text based adventure game
+
+![Spooky Manor](SpookyManor.png)
+
+[Finished Tutorial](sample.py)
---
+++ my-spookymanor.py
@@ -0,0 +1,24 @@
+from spookymanor.spookymanor import *
+
+# Define the rooms in your adventure here.
+# testRoom = Room("Sample Room", "Describe your room here!")
+
+# Connect all of the rooms together here.
+# testRoom.north = anotherRoom
+
+# Place the keys for your rooms here.
+# testRoom.add_key_for(anotherRoom)
+
+# Create your friends and where they will be hiding here.
+# testRoom.add_friend("Bing")
+
+# Define where the player will start the game here. (see Reference Code for how to do this)
+# set_current_room(testRoom)
+
+# Kick off the game here.
+# enter_the_manor()
+
+# Place your main game loop here. (It should run forever until the player either wins or loses.)
+# See the Reference Code for available options.
+while True:
+  print('Replace me')
